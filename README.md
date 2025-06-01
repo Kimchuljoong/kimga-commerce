5846f86eb6ae822c2a3e2520b93ab54f3414dc4f
Kimchuljoong <wnmh123@gmail.com>
2025-06-01 17:43:33 +0900

    Start E-Commerce multi module project with Kotlin

---
+++ README.md
@@ -0,0 +1,16 @@
+## Kimga E-Commerce Order Service
+
+### 모듈
+1. user(회원) Todo
+2. order(주문) Todo
+3. coupon(쿠폰) Todo
+4. common(공통) Todo
+5. db(데이터베이스) Todo
+---
+
+### 기술 스택
+- Kotlin
+- SpringBoot
+- JPA
+- Redis
+- Kafka
---
+++ src/test/kotlin/kr/co/kimga/commerce/CommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.kimga.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
