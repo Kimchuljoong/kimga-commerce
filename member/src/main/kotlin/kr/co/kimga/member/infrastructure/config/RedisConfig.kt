da8e9a54b34d51d45fd144e36f0c8715a5c8e9af
Kimchuljoong <wnmh123@gmail.com>
2025-06-05 08:14:20 +0900

    use redis with docker-compose

---
+++ member/src/main/kotlin/kr/co/kimga/member/infrastructure/config/RedisConfig.kt
@@ -0,0 +1,21 @@
+package kr.co.kimga.member.infrastructure.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        return RedisTemplate<String, Any>().apply {
+            connectionFactory = redisConnectionFactory
+            keySerializer = StringRedisSerializer()
+            valueSerializer = GenericJackson2JsonRedisSerializer()
+        }
+    }
+}
