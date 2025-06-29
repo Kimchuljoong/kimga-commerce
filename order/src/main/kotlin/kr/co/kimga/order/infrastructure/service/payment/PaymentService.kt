c1810c1a952f155a4cea5c2ae8f9f38c8d38341f
kimchj <wnmh123@naver.com>
2025-06-29 13:33:52 +0900

    impl createStock test

--- order/src/main/kotlin/kr/co/kimga/order/infrastructure/service/payment/PaymentService.kt
+++ order/src/main/kotlin/kr/co/kimga/order/infrastructure/service/payment/PaymentService.kt
@@ -1,11 +1,13 @@
 package kr.co.kimga.order.infrastructure.service.payment
 
 import kr.co.kimga.order.infrastructure.service.payment.pg.PaymentProcessor
+import lombok.RequiredArgsConstructor
 import org.springframework.stereotype.Service
 
 @Service
+@RequiredArgsConstructor
 class PaymentService(
-    List<PaymentProcessor> paymentProcessor
+    private val paymentProcessor: List<PaymentProcessor>
 ) {
 
 }
--- order/src/test/kotlin/kr/co/kimga/order/unit/StockServiceTest.kt
+++ order/src/test/kotlin/kr/co/kimga/order/unit/StockServiceTest.kt
@@ -1,13 +1,18 @@
 package kr.co.kimga.order.unit
 
+import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import kr.co.kimga.order.domain.entity.stock.Stock
 import kr.co.kimga.order.infrastructure.repository.StockJpaRepository
-import kr.co.kimga.order.infrastructure.service.order.OrderService
 import kr.co.kimga.order.infrastructure.service.stock.StockService
+import kr.co.kimga.order.infrastructure.service.stock.dto.RequestCreateStockDto
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
 
 @ExtendWith(MockKExtension::class)
 class StockServiceTest {
@@ -17,6 +22,37 @@ class StockServiceTest {
 
     @InjectMockKs
     private lateinit var stockService: StockService
+
+    @Test
+    @DisplayName("상품에 대한 재고를 생성할 수 있다")
+    fun `Can Create Stock for Product`() {
+        // given
+        val productId = 1L
+
+        val requestCreateStockDto = RequestCreateStockDto(
+            productId = productId
+        )
+
+        val fakeStock = Stock(
+            id = 1L,
+            productId = productId,
+        )
+
+        every {
+            stockJpaRepository.findStockByProductId(any())
+        } returns null
+
+        every {
+            stockJpaRepository.save(any())
+        } returns fakeStock
+
+        // when
+        stockService.createStock(requestCreateStockDto)
+
+        // then
+        verify { stockJpaRepository.findStockByProductId(any()) }
+        verify { stockJpaRepository.save(any()) }
+    }
 
 
 
