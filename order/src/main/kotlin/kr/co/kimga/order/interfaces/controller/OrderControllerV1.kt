193ef319903b0b8a9d578f6650cb812178d2e3c6
kimchj <wnmh123@naver.com>
2025-07-09 23:26:30 +0900

    impl findOrderDetails completeDelivery findOrders in OrderControllerV1

--- order/src/main/kotlin/kr/co/kimga/order/interfaces/controller/OrderControllerV1.kt
+++ order/src/main/kotlin/kr/co/kimga/order/interfaces/controller/OrderControllerV1.kt
@@ -2,6 +2,7 @@ package kr.co.kimga.order.interfaces.controller
 
 import kr.co.kimga.order.application.order.OrderFacade
 import kr.co.kimga.order.domain.entity.order.enums.OrderStatus
+import kr.co.kimga.order.infrastructure.service.order.dto.FindOrderDetailsDto
 import kr.co.kimga.order.infrastructure.service.order.dto.FindOrderDto
 import kr.co.kimga.order.infrastructure.service.order.dto.RequestCreateOrderDto
 import kr.co.kimga.order.infrastructure.service.order.dto.RequestFindOrdersDto
@@ -12,6 +13,9 @@ import org.springframework.data.domain.PageRequest
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequiredArgsConstructor
@@ -22,12 +26,16 @@ class OrderControllerV1(
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createOrder(@RequestBody request: RequestCreateOrderDto): Long {
+    fun createOrder(
+        @RequestBody request: RequestCreateOrderDto
+    ): Long {
         return orderFacade.createOrder(request)
     }
 
     @PostMapping("/{orderId}/cancel")
-    fun cancelOrder(@PathVariable("orderId") orderId: Long) {
+    fun cancelOrder(
+        @PathVariable("orderId") orderId: Long)
+    {
         orderFacade.cancelOrder(orderId)
     }
 
@@ -35,13 +43,35 @@ class OrderControllerV1(
     fun findOrders(
         @RequestHeader("X-User-Id") memberId: Long,
         @RequestParam("status") status: OrderStatus?,
-        @RequestParam from: Instant,
-        @RequestParam to: Instant,
+        @RequestParam from: String,
+        @RequestParam to: String,
         @RequestParam("page", defaultValue = "0") page: Int,
     ): Page<FindOrderDto> {
-        val requestFindOrdersDto = RequestFindOrdersDto(memberId, status, from, to)
-        val pageable = PageRequest.of(page, 10)
+        val fromDate = toInstant(from)
+        val toDate = toInstant(to)
+        val requestFindOrdersDto = RequestFindOrdersDto(memberId, status, fromDate, toDate)
+        val pageable = PageRequest.of(page, 50)
         return orderFacade.findOrders(requestFindOrdersDto, pageable)
+    }
+
+    @GetMapping("/{orderId}")
+    fun findOrderDetails(
+        @PathVariable("orderId") orderId: Long,
+    ): FindOrderDetailsDto {
+        return orderFacade.findOrderDetails(orderId)
+    }
+
+    @PostMapping("/{orderId}/delivery/complete")
+    fun completeDelivery(
+        @PathVariable orderId: Long
+    ) {
+        orderFacade.completeDelivery(orderId)
+    }
+
+    fun toInstant(str: String): Instant{
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val localDateTime = LocalDateTime.parse(str, formatter)
+        return localDateTime.atZone(ZoneId.of("Asia/Seoul")).toInstant()
     }
 
 }
