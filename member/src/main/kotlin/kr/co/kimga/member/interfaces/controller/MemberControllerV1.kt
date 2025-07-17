8eb48ec9d23d0711648ae18554ac248e041e85e2
kimchj <wnmh123@naver.com>
2025-07-17 23:51:54 +0900

    MemberControllerV1 Swagger apply

--- member/src/main/kotlin/kr/co/kimga/member/interfaces/controller/MemberControllerV1.kt
+++ member/src/main/kotlin/kr/co/kimga/member/interfaces/controller/MemberControllerV1.kt
@@ -1,5 +1,9 @@
 package kr.co.kimga.member.interfaces.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import kr.co.kimga.member.application.member.MemberFacade
 import kr.co.kimga.member.application.member.dto.JoinRequestDto
@@ -13,11 +17,18 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/member")
+@Tag(name = "회원 API V1")
 class MemberControllerV1(
     private val memberFacade: MemberFacade
 ) {
 
-    @PostMapping("/new")
+    @Operation(summary = "회원 생성")
+    @ApiResponses(
+        ApiResponse(responseCode = "201", description = "회원 신규 생성 정상"),
+        ApiResponse(responseCode = "409", description = "이미 생성된 회원 정보"),
+        ApiResponse(responseCode = "500", description = "내부 서버 오류")
+    )
+    @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@Valid @RequestBody memberCreateRequestDto: MemberCreateRequestDto) =
         memberFacade.join(JoinRequestDto(
@@ -26,10 +37,23 @@ class MemberControllerV1(
             name = memberCreateRequestDto.name,
         ))
 
+    @Operation(summary = "회원 정보 수정")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "회원 정보 수정 정상"),
+        ApiResponse(responseCode = "400", description = "수정 정보 정합성 문제"),
+        ApiResponse(responseCode = "404", description = "회원을 찾을 수 없음"),
+        ApiResponse(responseCode = "500", description = "내부 서버 오류")
+        )
     @PutMapping("")
     fun update(@Valid @RequestBody memberModifyRequestDto: MemberModifyRequestDto) =
         memberFacade.modify(ModifyRequestDto(MemberContextHolder.getContext().id, memberModifyRequestDto.name))
 
+    @Operation(summary = "회원 삭제")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "회원 삭제 정상"),
+        ApiResponse(responseCode = "404", description = "회원을 찾을 수 없음"),
+        ApiResponse(responseCode = "500", description = "내부 서버 오류")
+    )
     @DeleteMapping("")
     fun delete() {
         memberFacade.withdrawal(WithdrawalRequestDto(MemberContextHolder.getContext().id))
