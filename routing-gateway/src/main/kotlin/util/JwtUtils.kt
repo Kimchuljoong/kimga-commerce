8cd88767795a0f3ddfca94df7aff98bd0b364171
kimchj <wnmh123@naver.com>
2025-07-22 23:23:32 +0900

    make simple JwtUtils

---
+++ routing-gateway/src/main/kotlin/util/JwtUtils.kt
@@ -0,0 +1,34 @@
+package kr.co.kimga.routingGateway.util
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import java.time.LocalDateTime
+import javax.crypto.SecretKey
+
+class JwtUtil(secret: String) {
+
+    private val secretKey: SecretKey = Keys.hmacShaKeyFor(secret.toByteArray())
+
+    fun validateToken(token: String): Boolean {
+        return try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun getClaims(token: String): Claims {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+    }
+}
