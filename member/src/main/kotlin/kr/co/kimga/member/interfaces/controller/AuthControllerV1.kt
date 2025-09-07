807080b41010c2653afdd0d30f63254c73387734
Kimchuljoong <wnmh123@gmail.com>
2025-09-07 09:26:11 +0900

    refactoring member controller

--- member/src/main/kotlin/kr/co/kimga/member/interfaces/controller/AuthControllerV1.kt
+++ member/src/main/kotlin/kr/co/kimga/member/interfaces/controller/AuthControllerV1.kt
@@ -1,55 +1,35 @@
 package kr.co.kimga.member.interfaces.controller
 
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
-import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.validation.Valid
 import kr.co.kimga.member.application.auth.AuthFacade
 import kr.co.kimga.member.application.auth.dto.LoginRequestDto
-import kr.co.kimga.member.application.auth.dto.TokenDto
 import kr.co.kimga.member.application.auth.dto.LogoutRequestDto
 import kr.co.kimga.member.application.auth.dto.RefreshRequestDto
+import kr.co.kimga.member.application.auth.dto.TokenDto
 import kr.co.kimga.member.infrastructure.context.MemberContextHolder
 import kr.co.kimga.member.interfaces.controller.dto.AuthRefreshRequestDto
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/auth")
-@Tag(name = "인증 API V1")
-class AuthControllerV1 (
+class AuthControllerV1(
     private val authFacade: AuthFacade
-) {
+) : AuthApiV1 {
 
-    @Operation(summary = "회원 로그인")
-    @ApiResponses(
-        ApiResponse(responseCode = "200", description = "회원 로그인 성공"),
-        ApiResponse(responseCode = "404", description = "로그인 실패"),
-        ApiResponse(responseCode = "500", description = "내부 서버 오류")
-    )
-    @PostMapping("/login")
-    fun login(@Valid @RequestBody loginRequestDto: LoginRequestDto) : TokenDto {
+    override fun login(loginRequestDto: LoginRequestDto): TokenDto {
         return authFacade.login(loginRequestDto)
     }
 
-    @Operation(summary = "회원 토큰 갱신")
-    @ApiResponses(
-        ApiResponse(responseCode = "200", description = "토큰 갱신 성공"),
-        ApiResponse(responseCode = "400", description = "토큰이 유효하지 않음"),
-        ApiResponse(responseCode = "500", description = "내부 서버 오류")
-    )
-    @PutMapping("/refresh")
-    fun refresh(@Valid @RequestBody authRefreshRequestDto: AuthRefreshRequestDto) : TokenDto {
-        return authFacade.refresh(RefreshRequestDto(authRefreshRequestDto.refreshToken, MemberContextHolder.getContext().uuid))
+    override fun refresh(authRefreshRequestDto: AuthRefreshRequestDto): TokenDto {
+        return authFacade.refresh(
+            RefreshRequestDto(
+                authRefreshRequestDto.refreshToken,
+                MemberContextHolder.getContext().uuid
+            )
+        )
     }
 
-    @Operation(summary = "회원 로그아웃")
-    @ApiResponses(
-        ApiResponse(responseCode = "200", description = "회원 로그아웃 성공"),
-        ApiResponse(responseCode = "500", description = "내부 서버 오류")
-    )
-    @DeleteMapping("/logout")
-    fun logout() {
-        authFacade.logout(LogoutRequestDto(MemberContextHolder.getContext().uuid))
+    override fun logout() {
+        authFacade.logout(
+            LogoutRequestDto(MemberContextHolder.getContext().uuid)
+        )
     }
 }
--- member/src/main/kotlin/kr/co/kimga/member/interfaces/exception/GlobalExceptionHandler.kt
+++ member/src/main/kotlin/kr/co/kimga/member/interfaces/exception/GlobalExceptionHandler.kt
@@ -9,13 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException::class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun globalException(e: RuntimeException) : ExceptionResponse {
-        e.printStackTrace()
-        return e.message?.let { ExceptionResponse(it) } ?: ExceptionResponse()
-    }
-
     @ExceptionHandler(MemberNotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     fun memberNotFoundException(e: MemberNotFoundException) : ExceptionResponse = e.message?.let { ExceptionResponse(it) } ?: ExceptionResponse()
@@ -36,4 +29,7 @@ class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun accountBadRequestException(e: RuntimeException) : ExceptionResponse = e.message?.let { ExceptionResponse(it) } ?: ExceptionResponse()
 
+    @ExceptionHandler(RuntimeException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun globalException(e: RuntimeException) : ExceptionResponse = e.message?.let { ExceptionResponse(it) } ?: ExceptionResponse()
 }
